591b47ee3b85b665d3ef250054d64f3d847a53dd
Douglas Davis <douglasdavis@utexas.edu>
2014-02-02 21:33:43 -0600

    weird git stuff happening

--- DAnaLibs.hh
+++ DAnaLibs.hh
@@ -2,11 +2,8 @@
 #define DANALIBS_HH
 
 #include "TH1D.h"
-<<<<<<< HEAD
 #include "TPaveText.h"
-=======
 #include <iostream>
->>>>>>> 0df766ecb7e85ef0e9ec79e6d1469f10a1b9857c
 #include <vector>
 
 class Ana {
