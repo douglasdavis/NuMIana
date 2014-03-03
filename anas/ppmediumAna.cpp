879adf1052a36e6f0349e5501a92dad417bf4a7f
Douglas Davis <douglasdavis@utexas.edu>
2014-03-02 21:41:46 -0600

    opk

--- anas/ppmediumAna.cpp
+++ anas/ppmediumAna.cpp
@@ -15,31 +15,33 @@ int main(int argc, char *argv[])
 {
   looks();
   gStyle->SetOptStat(0);
-
+  
   std::string bott_file = argv[1];
   std::string long_file = argv[2];
   std::string norm_file = argv[3];
+  
   /*
-  numi::LArAna larbott(bott_file);
-  numi::LArAna larlong(long_file);
-  numi::LArAna larnorm(norm_file);
-
-  larbott.Print_ndecayppmedium(larbott.ppmediumndecay());
-  //  larlong.Print_ndecayppmedium(larlong.ppmediumndecay());
-  larnorm.Print_ndecayppmedium(larnorm.ppmediumndecay());
+    numi::LArAna larbott(bott_file);
+    numi::LArAna larlong(long_file);
+    numi::LArAna larnorm(norm_file);
+    
+    larbott.Print_ndecayppmedium(larbott.ppmediumndecay());
+    larlong.Print_ndecayppmedium(larlong.ppmediumndecay());
+    larnorm.Print_ndecayppmedium(larnorm.ppmediumndecay());
   */
+  
   numi::simpleAna sbott(bott_file,false);
   numi::simpleAna slong(long_file,false);
   numi::simpleAna snorm(norm_file,true);
-
+  
   numi::utils::Print_ndecayppmedium(sbott.ppmediumndecay(),
-		       sbott.ndecayCodeMap(),
-		       sbott.ppmediumCodeMap());
+				    sbott.ndecayCodeMap(),
+				    sbott.ppmediumCodeMap());
   numi::utils::Print_ndecayppmedium(slong.ppmediumndecay(),
-		       slong.ndecayCodeMap(),
-		       slong.ppmediumCodeMap());
+				    slong.ndecayCodeMap(),
+				    slong.ppmediumCodeMap());
   numi::utils::Print_ndecayppmedium(snorm.ppmediumndecay(),
-		       snorm.ndecayCodeMap(),
-		       snorm.ppmediumCodeMap());
+				    snorm.ndecayCodeMap(),
+				    snorm.ppmediumCodeMap());
   return 0;
 }
