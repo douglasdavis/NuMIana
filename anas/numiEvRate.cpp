3b1434c8ebe74bc5ac25a10deee81f8d555a74c8
Doug Davis <douglasdavis@utexas.edu>
2014-03-10 11:20:01 -0500

    naming

--- anas/numiEvRate.cpp
+++ anas/numiEvRate.cpp
@@ -19,25 +19,25 @@ int main(int argc, char *argv[])
   infiles.close();
 
   numi::EvRate *bottom = new numi::EvRate(dir_bottom,genie_file,file_list,true,false,false);
-  bottom->MakeHists("DATA_NEW/bottom_2013.root",bottom->BottomArea());
+  bottom->MakeHists("histogram_files/bottom_2013.root",bottom->BottomArea());
   delete bottom;
 
   numi::EvRate *length = new numi::EvRate(dir_length,genie_file,file_list,false,true,false);
-  length->MakeHists("DATA_NEW/length_2013.root",length->LengthArea());
+  length->MakeHists("histogram_files/length_2013.root",length->LengthArea());
   delete length;
 
   numi::EvRate *normal = new numi::EvRate(dir_normal,genie_file,file_list,false,false,true);
-  normal->MakeHists("DATA_NEW/normal_2013.root",normal->NormalArea());
+  normal->MakeHists("histogram_files/normal_2013.root",normal->NormalArea());
   delete normal;
 
   numi::EvRate *bcut = new numi::EvRate(dir_bottom,genie_file,file_list,true,false,false);
   bcut->SetLowerVzCut(7e4);
-  bcut->MakeHists("DATA_NEW/bottom_2013_dump_cut.root",bcut->BottomArea());
+  bcut->MakeHists("histogram_files/bottom_2013_dump_cut.root",bcut->BottomArea());
   delete bcut;
 
   numi::EvRate *lcut = new numi::EvRate(dir_length,genie_file,file_list,false,true,false);
   lcut->SetLowerVzCut(7e4);
-  lcut->MakeHists("DATA_NEW/length_2013_dump_cut.root",lcut->LengthArea());
+  lcut->MakeHists("histogram_files/length_2013_dump_cut.root",lcut->LengthArea());
   delete lcut;
 
   return 0;
